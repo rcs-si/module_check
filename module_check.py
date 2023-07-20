c947e95227775564cf22158a813175851a0a0cc8
Brian Gregor <bgregor@bu.edu>
2023-07-20 13:46:46 -0400

    Update module_check.py

--- module_check.py
+++ module_check.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
-import command_line
+#import command_line
 import lua_parse
+import module_env
+import argparse
+
 
 module_path = str
 def main():
-    command_line.parse()
+    #command_line.parse()
+    # Step 1
+    # run module_env funcs to get a list of environment variables
+    # get 2 lists: module help list, module load list
+    # then compare them, maybe put compare functions in a new .py file
+    # If this fails, stop
     
 
 
