f31ec95474cbeb2a301e20d2f6c770177e57d3ca
sdallman <sdallman@bu.edu>
2023-07-20 13:03:06 -0400

    added new file

---
+++ module_show_parse.py
@@ -0,0 +1,10 @@
+# module_show_parse
+
+import subprocess
+
+def module_show(x):
+    result = subprocess.run(['module show', x], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    print(result)
+    # subprocess.call(["bash", "module show " + x])
+
+module_show("bwa/0.7.17")
