e6368d299e4eb7802d48ffa61b49884bfb6b3857
Reetom Gangopadhyay <rgangopa@bu.edu>
2024-03-08 14:06:37 -0500

    implemented changes from meeting, changed location of is_module_published fn to before split

--- command_line.py
+++ command_line.py
@@ -7,6 +7,14 @@ Should handle whether there are leading '/' since /abc/1.2.3 would return versio
 import argparse
 from pathlib import Path
 import subprocess
+import os
+
+DEFAULT_PATH = "/share/pkg.8/"
+
+def is_module_published(module_name, module_version):
+    command = f"module -t -r avail {module_name}/{module_version}"
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    return len(stderr) > 0
 
 def split(module):
     parts = module.split('/')
@@ -20,36 +28,36 @@ def split(module):
     module_name, module_version = parts[0], parts[1]
     return module_name, module_version
 
-
-def is_module_published(module_name, module_version):
-    command = f"module -t -r avail {module_name}/{module_version}"
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    return "ModuleNotFoundError" not in result.stderr
-
 def parse():
     argParser = argparse.ArgumentParser(description="Check modules for possible problems.")
-    argParser.add_argument("-c", "--check", type=str, help="the name of module to be checked")
+    argParser.add_argument("-c", "--check", type=str, help="the name of module to be checked", required=True)
     argParser.add_argument("-p", "--path", type=str, help="path to the module")
 
     args = argParser.parse_args()
 
-    if not args.check or not args.path:
-        argParser.print_help()
-        raise Exception("No module to check. Provide either the module name or the path to the module.")
+    # if not args.check or not args.path:
+    #     argParser.print_help()
+    #     raise Exception("No module to check. Provide either the module name or the path to the module.")
+
+
+    module_path_str = os.path.join(DEAFULT_PATH,args.check)
 
     if args.path:
-        module_path_str = "/share/pkg.8/" + args.check
-        module_path = Path(module_path_str)
+        os.path.join(args.path,args.check)
+
+    # if args.path: # default to share/pkg.8 for now (see var "DEFAULT_PATH")
+    #     module_path_str = "/share/pkg.8/" + args.check
+    #     module_path = Path(module_path_str)
 
         if not module_path.exists():
             print("The target directory doesn't exist")
             raise SystemExit(1)
 
-    if args.check:
-        module_name, module_version = split(args.check)
-        if not is_module_published(module_name, module_version):
-            print(f"The module {args.check} is not published.")
-            raise SystemExit(1)
+
+    module_name, module_version = split(args.check)
+    if not is_module_published(module_name, module_version):
+        print(f"The module {args.check} is not published.")
+        #raise SystemExit(1)
 
     return args.check
 
